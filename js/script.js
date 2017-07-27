51d610727c01771311bff7e2e710f0f7ef65aece
L9gc2pa <ngvrz@bk.ru>
2017-07-27 23:38:02 +0500

    Edits maps, etc.

--- js/script.js
+++ js/script.js
@@ -51,24 +51,64 @@ if (contest) {
   });
 };
 
+// ymaps.ready(function () {
+//   var myMap = new ymaps.Map("map", {
+//     center: [59.9389231, 30.323055],
+//     zoom: 16,
+//     controls: ["smallMapDefaultSet"]
+//   }, {
+//     searchControlProvider: "yandex#search"
+//   }),
+//   myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+//     hintContent: "Pink"
+//   }, {
+//     cursor: "arrow",
+//     iconLayout: "default#image",
+//     iconImageHref: "img/icon-map-marker.svg",
+//     iconImageSize: [36, 36],
+//     iconImageOffset: [-18,-18],
+//     hasBalloon: false
+//   });
+//   myMap.geoObjects.add(myPlacemark);
+// });
 
-ymaps.ready(function () {
-  var myMap = new ymaps.Map("map", {
-    center: [59.9389231, 30.323055],
-    zoom: 16,
-    controls: ["smallMapDefaultSet"]
-  }, {
-    searchControlProvider: "yandex#search"
-  }),
-  myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-    hintContent: "Pink"
-  }, {
-    cursor: "arrow",
-    iconLayout: "default#image",
-    iconImageHref: "img/icon-map-marker.svg",
-    iconImageSize: [36, 36],
-    iconImageOffset: [-18,-18],
-    hasBalloon: false
-  });
-  myMap.geoObjects.add(myPlacemark);
-});
+var mapElement = document.getElementById("map");
+if (mapElement) {
+  var map ="";
+  google.maps.event.addDomListener(window, "load", init);
+  google.maps.event.addDomListener(window, "resize", m_res );
+  function init() {
+      var mapOptions = {
+        zoom: 15,
+        mapTypeControl: false,
+        zoomControl: true,
+        scrollwheel: false,
+        zoomControlOptions: {position: google.maps.ControlPosition.LEFT_CENTER},
+      streetViewControl: false,
+      center: new google.maps.LatLng(59.936287, 30.321047),
+    };
+
+    mapElement.classList.add("contacts-map--map-loaded");
+    map = new google.maps.Map(mapElement, mapOptions);
+    var image = {
+      url: "img/icon-map-marker.svg",
+      size: new google.maps.Size(36, 36),
+      scaledSize: new google.maps.Size(36, 36),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(18, 18)
+    };
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(59.936287, 30.321047),
+      map: map,
+      optimized: false,
+      icon: image
+    });
+    m_res();
+  }
+
+  function m_res(){
+    google.maps.event.trigger(map, "resize");
+    map.panTo(new google.maps.LatLng(59.936287, 30.321047));
+  }
+}
